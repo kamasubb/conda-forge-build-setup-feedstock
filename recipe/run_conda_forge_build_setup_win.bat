5fd7e11fc0209e402caada5425e657ea4aa4137d
Isuru Fernando <isuru.11@cse.mrt.ac.lk>
2017-01-06 11:52:12 +0530

    Install vs2008_express_vc_python_patch on appveyor

--- recipe/run_conda_forge_build_setup_win.bat
+++ recipe/run_conda_forge_build_setup_win.bat
@@ -11,5 +11,9 @@ conda config --set add_pip_as_python_dependency false
 conda update -n root --yes --quiet conda conda-env conda-build
 conda install -n root --yes --quiet jinja2 conda-build anaconda-client
 
+:: Needed for building extensions in python2.7 x64 with cmake.
+:: Since python version and arch is not known at this point, install it everywhere.
+conda install -n root --yes --quiet vs2008_express_vc_python_patch
+
 conda info
 conda config --get
